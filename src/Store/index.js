d2766490c7015f32ff1fa9d9d040545e181a26ac
Murtaza Mehmood <murtazamahmood640@gmail.com>
2025-05-22 15:43:38 +0500

    profile management merged

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
+import authReducer from "./authSlice";
+ 
  
 const store = configureStore({
   reducer: {
-    user: userReducer,
+     auth: authReducer,
   },
 });
  
 export default store;
+ 
  
  
